4e940f51486fdf6d131d8f91b7e10fd1644cb714
Wallauerr <wallauer@protonmail.com>
2025-01-27 16:54:20 -0300

    feat: add number extract, remove unusable info and add font size

--- src/extract_info_and_create_pdf.py
+++ src/extract_info_and_create_pdf.py
@@ -19,21 +19,17 @@ def extract_info_and_create_pdf(xml_file, pdf_file):
   # Define o namespace
   namespace = {"nfe": "http://www.portalfiscal.inf.br/nfe"}
 
+  # Extrair numero
+  ide_element = root.find(".//nfe:ide", namespaces=namespace)
+  nfe_info = {}
+  if ide_element is not None:
+    nfe_info["numero"] = ide_element.find(".//nfe:nNF", namespaces=namespace).text
+
   # Extrair emitente
   emit_element = root.find(".//nfe:emit", namespaces=namespace)
   emit_info = {}
   if emit_element is not None:
-    emit_info["CNPJ"] = emit_element.find(".//nfe:CNPJ", namespaces=namespace).text
     emit_info["xNome"] = emit_element.find(".//nfe:xNome", namespaces=namespace).text
-    endereco_emit = emit_element.find(".//nfe:enderEmit", namespaces=namespace)
-    emit_info["endereco"] = {
-      "xLgr": endereco_emit.find(".//nfe:xLgr", namespaces=namespace).text,
-      "nro": endereco_emit.find(".//nfe:nro", namespaces=namespace).text,
-      "xBairro": endereco_emit.find(".//nfe:xBairro", namespaces=namespace).text,
-      "xMun": endereco_emit.find(".//nfe:xMun", namespaces=namespace).text,
-      "UF": endereco_emit.find(".//nfe:UF", namespaces=namespace).text,
-      "CEP": endereco_emit.find(".//nfe:CEP", namespaces=namespace).text,
-    }
 
   # Extrair destinatário
   dest_element = root.find(".//nfe:dest", namespaces=namespace)
@@ -48,16 +44,13 @@ def extract_info_and_create_pdf(xml_file, pdf_file):
       "xBairro": endereco_dest.find(".//nfe:xBairro", namespaces=namespace).text,
       "xMun": endereco_dest.find(".//nfe:xMun", namespaces=namespace).text,
       "UF": endereco_dest.find(".//nfe:UF", namespaces=namespace).text,
-      "CEP": endereco_dest.find(".//nfe:CEP", namespaces=namespace).text,
     }
 
   # Extrair transportadora
   transp_element = root.find(".//nfe:transporta", namespaces=namespace)
   transp_info = {}
   if transp_element is not None:
-    transp_info["CNPJ"] = transp_element.find(".//nfe:CNPJ", namespaces=namespace).text
     transp_info["xNome"] = transp_element.find(".//nfe:xNome", namespaces=namespace).text
-    transp_info["endereco"] = transp_element.find(".//nfe:xEnder", namespaces=namespace).text
 
   # Extrair volumes
   volume_element = root.find(".//nfe:vol", namespaces=namespace)
@@ -67,34 +60,37 @@ def extract_info_and_create_pdf(xml_file, pdf_file):
 
   # Criar PDF com informações extraídas
   c = canvas.Canvas(pdf_file, pagesize=letter)
-  c.drawString(250, 750, f"NOME DA EMPRESA")
+
+  # Numero
+  c.setFontSize(16)
+  c.drawString(250, 750, f"{nfe_info['numero']}")
 
   # Emit
-  c.drawString(100, 730, f"Emitente:")
-  c.drawString(100, 710, f"CNPJ: {emit_info['CNPJ']}")
+  c.setFontSize(12)
   c.drawString(100, 690, f"Nome: {emit_info['xNome']}")
-  c.drawString(100, 670, f"Endereço: {emit_info['endereco']['xLgr']}, {emit_info['endereco']['nro']}")
-  c.drawString(100, 650, f"Bairro: {emit_info['endereco']['xBairro']}")
-  c.drawString(100, 630, f"Cidade: {emit_info['endereco']['xMun']} - {emit_info['endereco']['UF']}")
-  c.drawString(100, 610, f"CEP: {emit_info['endereco']['CEP']}")
 
   # Dest
+  c.setFontSize(12)
   c.drawString(100, 590, f"Destinatário:")
+  c.setFontSize(12)
   c.drawString(100, 570, f"CNPJ: {dest_info['CNPJ']}")
+  c.setFontSize(12)
   c.drawString(100, 550, f"Nome: {dest_info['xNome']}")
+  c.setFontSize(12)
   c.drawString(100, 530, f"Endereço: {dest_info['endereco']['xLgr']}, {dest_info['endereco']['nro']}")
+  c.setFontSize(12)
   c.drawString(100, 510, f"Bairro: {dest_info['endereco']['xBairro']}")
+  c.setFontSize(12)
   c.drawString(100, 490, f"Cidade: {dest_info['endereco']['xMun']} - {dest_info['endereco']['UF']}")
-  c.drawString(100, 470, f"CEP: {dest_info['endereco']['CEP']}")
 
   # Transportadora
+  c.setFontSize(12)
   c.drawString(100, 450, f"Transportadora:")
-  c.drawString(100, 430, f"CNPJ: {transp_info['CNPJ']}")
+  c.setFontSize(12)
   c.drawString(100, 410, f"Nome: {transp_info['xNome']}")
-  c.drawString(100, 390, f"Endereço: {transp_info['endereco']}")
 
   # Volumes
-  c.drawString(100, 370, f"Volumes:")
-  c.drawString(100, 350, f"Quantidade de Volumes: {vol_info['qVol']}")
+  c.setFontSize(12)
+  c.drawString(100, 350, f"{vol_info['qVol']}")
 
   c.save()
